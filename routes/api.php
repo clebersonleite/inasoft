77ed38c3b556ce7454acadff545ce14ed7a7d661
Cleberson Leite <kleberson1992@gmail.com>
2024-01-16 21:07:09 -0300

    Start Project

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+// use Illuminate\Http\Request;
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('users', UserController::class);
+
+// Route::delete('/users/{id}', [UserController::class, 'destroy']);
+// Route::patch('/users/{id}', [UserController::class, 'update']);
+// Route::get("/users/{id}", [UserController::class, 'show']);
+// Route::get("/users", [UserController::class, 'index']);
+// Route::post("/users", [UserController::class, 'store']);
+
+
+Route::get('/', function () {
+    return "Hello World";
+});
